602fca9a39c410f6d03a7d5713dd380bc1883a38
ABDELLATYF EN-NEIYMY <rca8272@gmail.com>
2023-08-11 17:43:02 +0100

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-# include <stdio.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/*betty style doc for function main goes there */
-void	check_neg_pos(int num);
-
-int main(void) 
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2; /**get random number*/
-	check_neg_pos(n); /**check the number*/
-	/* your code goes there */
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
-}
-
-void	check_neg_pos(int num) /*function to check positive number or negative*/
-	{
-	if (num < 0)/**check the number is less than zero*/
-		printf("%d is negative\n", num);
-	else if (num > 0) /**check the number is more than zero*/
-		printf("%d is positive\n", num);
-	else /**check the number is equal zero*/
-		printf("%d is zero\n", num);
 }
